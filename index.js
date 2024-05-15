07685b943e5c9ba6588689be73b7ae716c62d5bd
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-05-15 16:50:00 +0600

    clear token on logout

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -40,10 +41,27 @@ async function run() {
     // jwt Generate
     app.post('/jwt', async(req, res) =>{
       const user = req.body;
+      console.log(user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn:'365d'
       })
+      res.cookie('token', token, {
+        httpOnly:true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      })
+      .send({success: true})
+    })
 
+    // clear token on logout
+    app.get('/logout', (req, res) =>{
+      res.clearCookie('token',  {
+        httpOnly:true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        maxAge: 0,
+      })
+      .send({success: true})
     })
 
     // get all data 
